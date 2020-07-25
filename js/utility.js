f8f022385057643b1cdf34506b5653ff4ce487b1
Shrijan Bajracharya <shrijan.buzz.5@gmail.com>
2020-07-25 16:11:46 +0545

    Add function to add uploaded images to gallary.

--- js/utility.js
+++ js/utility.js
@@ -1,9 +1,28 @@
 const fadeIn = (element) => {
   element.classList.add("fade-in");
   element.classList.remove("fade-out");
+  element.style.visibility = "hidden";
 };
 
 const fadeOut = (element) => {
   element.classList.add("fade-out");
   element.classList.remove("fade-in");
+  element.style.visibility = "hidden";
+};
+
+// Add new image in gallary
+const addImage = (uploadSrc) => {
+  let imageContainer = document.createElement("div");
+  imageContainer.classList.add("img-container");
+  let gallaryImage = document.createElement("img");
+  gallaryImage.classList.add("gallary-image");
+  gallaryImage.setAttribute("src", uploadSrc);
+  imageContainer.appendChild(gallaryImage);
+  gallary.appendChild(imageContainer);
+  gallaryImage.addEventListener("click", function () {
+    newSource = gallaryImage.getAttribute("src");
+    cardImage.src = newSource;
+    fadeIn(detailView);
+    detailView.style.visibility = "visible";
+  });
 };
